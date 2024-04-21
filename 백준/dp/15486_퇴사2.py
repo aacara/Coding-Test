432de9e9cf3f8b8ae1561f39e88756b6fe05d627
aacara <budgielover@naver.com>
2024-04-21 22:48:29 +0900

    2024-04-21 문제

---
+++ 백준/dp/15486_퇴사2.py
@@ -0,0 +1,18 @@
+N = int(input())
+dp = [0] * (N+1)
+days = []
+prices = []
+
+for i in range(N):
+  t, p = map(int, input().split())
+  days.append(t)
+  prices.append(p)
+
+for i in range(N):
+  # 상담을 하는 경우
+  if i+days[i] <= N:
+    dp[i+days[i]] = max(dp[i]+prices[i], dp[i+days[i]])
+  # 상담을 하지 않는 경우 - 이부분 놓침
+  if i < N:
+    dp[i+1] = max(dp[i+1], dp[i])
+print(max(dp))
